108e9fd7a425dcd69dcc71e682427f476c16d4ae
Caroline Goovaerts <caroline@sourcequality.ch>
2022-04-26 15:57:14 +0200

    Limit accessibility

--- java11/src/test/java/module-info.java
+++ java11/src/test/java/module-info.java
@@ -1,5 +1,5 @@
-open module ch.sourcequality.poc.java_eleven.jigsaw {
+module ch.sourcequality.poc.java_eleven.jigsaw {
     requires ch.sourcequality.poc.jigsaw;
     requires org.junit.jupiter.api;
-//    opens ch.sourcequality.poc.java_eleven.jigsaw to org.junit.platform.commons;
+    opens ch.sourcequality.poc.java_eleven.jigsaw to org.junit.platform.commons;
 }
