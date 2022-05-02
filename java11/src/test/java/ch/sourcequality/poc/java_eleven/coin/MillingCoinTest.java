84e8aa1d466d02733935ff480868ac29d7b0d47a
Caroline Goovaerts <caroline@sourcequality.ch>
2022-05-02 14:06:36 +0200

    Demo resource references and reformat code

--- java11/src/test/java/ch/sourcequality/poc/java_eleven/coin/MillingCoinTest.java
+++ java11/src/test/java/ch/sourcequality/poc/java_eleven/coin/MillingCoinTest.java
@@ -2,13 +2,22 @@ package ch.sourcequality.poc.java_eleven.coin;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class MillingCoinTest {
+
+    private static String firstLineFromFile(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        try (reader) {
+            return reader.readLine();
+        }
+    }
 
     @Test
     void shouldCompileWithoutWarnings() {
@@ -19,7 +28,18 @@ public class MillingCoinTest {
         assertEquals(List.of(1, 2), collection);
     }
 
-    void demoTryWithResourcesWithFinalVariable() {
-        fail("not yet implemented");
+    @Test
+    void demoTryWithResourcesWithFinalVariable() throws Exception {
+        String resource = "/lorem-ipsum.txt";
+        String expected = "Lorem ipsum ...";
+
+        String firstLine = firstLineFromFile(pathToResource(resource));
+
+        assertEquals(expected, firstLine);
+    }
+
+    @SuppressWarnings("all")
+    private String pathToResource(String resource) {
+        return this.getClass().getResource(resource).getPath();
     }
 }
